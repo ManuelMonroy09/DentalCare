9dd55e9ad15415a4f177714b8eb80a9cf48009ba
ManuelMonroy09 <129925156+ManuelMonroy09@users.noreply.github.com>
2026-09-04 11:28:06 -0600

    security: strengthen encryption constants for login-based key protection

--- src/main/java/mx/dentalcare/security/EncryptionConstants.java
+++ src/main/java/mx/dentalcare/security/EncryptionConstants.java
@@ -1,11 +1,26 @@
 package mx.dentalcare.security;
 
-public class EncryptionConstants {
-    private EncryptionConstants(){}
+public final class EncryptionConstants {
+
+    private EncryptionConstants() {
+    }
+
     public static final int SALT_LENGTH = 16;
     public static final int IV_LENGTH = 12;
     public static final int KEY_LENGTH = 256;
-    public static final int ITERATIONS= 65536;
+
+    /** KDF utilizado por el esquema actual. */
+    public static final int KDF_ITERATIONS = 600_000;
+
+    /** Solo para leer archivos creados por el esquema anterior. */
+    public static final int LEGACY_ITERATIONS = 65_536;
+
     public static final String KEY_ALGORITHM = "PBKDF2WithHmacSHA256";
     public static final String CIPHER = "AES/GCM/NoPadding";
+
+    public static final int GCM_TAG_LENGTH = 128;
+
+    public static final int MASTER_KEY_LENGTH_BYTES = 32;
+
+    public static final int SECURITY_VERSION = 1;
 }

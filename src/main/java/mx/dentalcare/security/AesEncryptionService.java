0502f7d2cd3c0b5a62c8489b8002590f8e237b7b
ManuelMonroy09 <129925156+ManuelMonroy09@users.noreply.github.com>
2026-09-04 11:28:16 -0600

    security: centralize AES-GCM authentication tag configuration

--- src/main/java/mx/dentalcare/security/AesEncryptionService.java
+++ src/main/java/mx/dentalcare/security/AesEncryptionService.java
@@ -6,27 +6,26 @@ import javax.crypto.spec.GCMParameterSpec;
 
 public class AesEncryptionService {
 
-    private static final int TAG_LENGTH = 128;
+    private static final int TAG_LENGTH = EncryptionConstants.GCM_TAG_LENGTH;
 
-    public byte[] encrypt(byte[] data, SecretKey key, byte[] iv){
-        try{
+    public byte[] encrypt(byte[] data, SecretKey key, byte[] iv) {
+        try {
             Cipher cipher = Cipher.getInstance(EncryptionConstants.CIPHER);
             GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH, iv);
             cipher.init(Cipher.ENCRYPT_MODE, key, spec);
             return cipher.doFinal(data);
-        }catch (Exception e){
-            throw new RuntimeException("Erroral cifrar datos.", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al cifrar datos.", e);
         }
     }
 
-    public byte[] decrypt(byte[] encryptedData, SecretKey key, byte[] iv){
+    public byte[] decrypt(byte[] encryptedData, SecretKey key, byte[] iv) {
         try {
-
             Cipher cipher = Cipher.getInstance(EncryptionConstants.CIPHER);
             GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH, iv);
             cipher.init(Cipher.DECRYPT_MODE, key, spec);
             return cipher.doFinal(encryptedData);
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException("Error al descifrar datos.", e);
         }
     }

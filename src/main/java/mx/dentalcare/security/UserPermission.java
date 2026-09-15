e2e81fb7d96798388216a19ee05e984a6082ef06
ManuelMonroy09 <129925156+ManuelMonroy09@users.noreply.github.com>
2026-09-14 21:40:04 -0600

    Agregar permisos base del sistema de usuarios

---
+++ src/main/java/mx/dentalcare/security/UserPermission.java
@@ -0,0 +1,14 @@
+package mx.dentalcare.security;
+
+public enum UserPermission {
+    VER_INICIO,
+    VER_PACIENTES,
+    GESTIONAR_CITAS,
+    VER_TRATAMIENTOS,
+    VER_HISTORIAL,
+    VER_FINANZAS,
+    VER_CONFIGURACION,
+    GESTIONAR_USUARIOS,
+    VER_ROLES,
+    VER_AUDITORIA
+}
